8e3e06ee1c9bc8abcae1f6694fbeda07e5de57f0
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-04-05 19:48:22 +0300

    Create defaultSettings.js

---
+++ src/settings/defaultSettings.js
@@ -0,0 +1,95 @@
+let defaultSettings = {
+  'working mode': {
+    'type': 'panel',
+  },
+  'where to react': {
+    'popular': true,
+    'new': true,
+    'my new': true,
+    'subsites': true,
+    'user pages': true,
+    'topics': true
+  },
+  'what to show in': {
+    'popular': 'blogs and subsites',
+    'new': 'blogs and subsites',
+    'my new': 'blogs and subsites',
+    'subsites': 'blogs and subsites',
+    'user pages': 'blogs and subsites',
+    'topics': 'blogs and subsites'
+  },
+  'what to group': {
+    'subsites': true,
+    'blogs': true
+  },
+  'filters': {
+    'subsites': {
+      'title': {
+        'active': true,
+        'what to do': 'collapse',
+        'title': {
+          'react title': true,
+          'react no title': true,
+          'words': []
+        },
+        'text': {
+          'react text': true,
+          'react no text': true,
+          'words': []
+        }
+      }
+    },
+    'blogs': {
+      'title': {
+        'active': true,
+        'what to do': 'collapse',
+        'title': {
+          'react title': true,
+          'react no title': true,
+          'words': []
+        },
+        'text': {
+          'react text': true,
+          'react no text': true,
+          'words': []
+        }
+      }
+    }
+  },
+  'what with': {
+    'ignored': {
+      'feeds': {
+        'action': 'collapse',
+        'color': ''
+      },
+      'authors': {
+        'action': 'collapse',
+        'color': ''
+      },
+      'subsites': {
+        'action': 'collapse',
+        'color': ''
+      }
+    },
+    'watched': {
+      'feeds': {
+        'action': 'collapse',
+        'color': ''
+      }
+    }
+  },
+  'data': {
+    'watched feeds': [],
+    'ignored feeds': [],
+    'favorite authors': [],
+    'ignored authors': [],
+    'ignored subsites': [],
+    'favorite subsites': []
+  },
+  'scriptInfo': {
+    scriptName: 'DTF markdown',
+    scriptId: 'dtf-markdown',
+    storeName: 'DTF-markdown',
+    storeDesc: 'Скрипт для расширения возможностей, десу!'
+  }
+}
