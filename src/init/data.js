53d4412396f0a92afd3ce8542b0a873b3268d645
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-04-13 11:24:41 +0300

    Update data.js

--- src/init/data.js
+++ src/init/data.js
@@ -15,11 +15,13 @@ initMenu.setData = (main, mainCfg) => {
     function feedOpener(i){
       window.open(`https://dtf.ru/${i.feedID}`, '_blank').focus();
     };
+    
+    // Игнорируемые
     new Field({
       path: main,
       groupName: 'script data',
       cName: 'flex',
-      legend: 'Игнорируемые',
+      legend: '💢 Игнорируемые',
       info: 'Подсайты',
       rtn: [],
       liveList: {c:{g:'ignored'}, a:mainCfg,
@@ -56,7 +58,7 @@ initMenu.setData = (main, mainCfg) => {
       path: main,
       groupName: 'script data',
       cName: 'flex',
-      legend: 'Игнорируемые',
+      legend: '💢 Игнорируемые',
       info: 'Авторы',
       rtn: [],
       liveList: {c:{g:'ignored'}, a:mainCfg,
@@ -93,7 +95,7 @@ initMenu.setData = (main, mainCfg) => {
       path: main,
       groupName: 'script data',
       cName: 'flex',
-      legend: 'Игнорируемые',
+      legend: '💢 Игнорируемые',
       info: 'Фиды',
       rtn: [],
       liveList: {c:{g:'ignored'}, a:mainCfg,
@@ -136,4 +138,126 @@ initMenu.setData = (main, mainCfg) => {
         }]
       }
     });
+    // Избранные
+    new Field({
+      path: main,
+      groupName: 'script data',
+      cName: 'flex',
+      legend: '💘 Избранные',
+      info: 'Подсайты',
+      rtn: [],
+      liveList: {c:{g:'favorites'}, a:mainCfg,
+        list:[
+        {
+          type: 'object',
+          mode: 'view-del',
+          label: 'Подсайты',
+          name: 'subsites',
+          cName: 'vertical',
+          view: (e) => {
+              if(!e) return;
+              return `📛: ${e.author},
+              🆔: ${e.authorID},
+              ❓: ${e.authorType}`
+          },
+          buttons: (e, v) => {
+              if(!e) return;
+              new Button({
+                  path: e,
+                  cName: 'btn',
+                  text: '🔗',
+                  onclick: () => {
+                      linkOpener(v);
+                  }
+              })
+          },
+          clearList: true
+        }]
+      }
+    });
+    
+    new Field({
+      path: main,
+      groupName: 'script data',
+      cName: 'flex',
+      legend: '💘 Избранные',
+      info: 'Авторы',
+      rtn: [],
+      liveList: {c:{g:'favorites'}, a:mainCfg,
+        list:[
+        {
+          type: 'object',
+          mode: 'view-del',
+          label: 'Авторы',
+          name: 'authors',
+          cName: 'vertical',
+          view: (e) => {
+              if(!e) return;
+              return `📛: ${e.author},
+              🆔: ${e.authorID},
+              ❓: ${e.authorType}`
+          },
+          buttons: (e, v) => {
+              if(!e) return;
+              new Button({
+                  path: e,
+                  cName: 'btn',
+                  text: '🔗',
+                  onclick: () => {
+                      linkOpener(v);
+                  }
+              })
+          },
+          clearList: true
+        }]
+      }
+    });
+    
+//     new Field({
+//       path: main,
+//       groupName: 'script data',
+//       cName: 'flex',
+//       legend: '💘 Избранные',
+//       info: 'Фиды',
+//       rtn: [],
+//       liveList: {c:{g:'favorites'}, a:mainCfg,
+//         list:[
+//         {
+//           type: 'object',
+//           mode: 'view-del',
+//           label: 'Фиды',
+//           name: 'feeds',
+//           cName: 'vertical',
+//           view: (e) => {
+//               if(!e) return;
+//               return `📰🆔: ${e.feedID};
+//               📰📜 ${e.feedTitle};
+//               📛: ${e.author};
+//               🆔: ${e.authorID};
+//               ❓: ${e.authorType};
+//               📅: ${e.date};`
+//           },
+//           buttons: (e, v) => {
+//               if(!e) return;
+//               new Button({
+//                   path: e,
+//                   cName: 'btn',
+//                   text: '🔗',
+//                   onclick: () => {
+//                       linkOpener(v);
+//                   }
+//               });
+//               new Button({
+//                   path: e,
+//                   cName: 'btn',
+//                   text: '🔗',
+//                   onclick: () => {
+//                       feedOpener(v);
+//                   }
+//               })
+//           },
+//           clearList: true
+//         }]
+//       }
+//     });
   }
