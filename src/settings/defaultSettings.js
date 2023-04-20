e4958078844bbb33e28c7a0caa1bc4d38a4089b1
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-04-20 13:24:29 +0300

    Update defaultSettings.js

--- src/settings/defaultSettings.js
+++ src/settings/defaultSettings.js
@@ -1,26 +1,100 @@
 const defaultSettings = {
   'working mode': {
     'type': 'panel',
-  },
-  'where to react': {
-    'popular': true,
-    'new': true,
-    'my new': true,
-    'bookmarks': false,
-    'subsites': true,
-    'user pages': true,
-    'topics': true,
-    'types to show': {
-      'popular': 'subsites and blogs',
-      'new': 'subsites and blogs',
-      'my new': 'subsites and blogs',
-      'bookmarks': 'subsites and blogs',
-      'subsites': 'subsites and blogs',
-      'user pages': 'subsites and blogs',
-      'topics': 'subsites and blogs'
-    },
+    'where to react': {
+      'popular': true,
+      'new': true,
+      'my new': true,
+      'bookmarks': false,
+      'subsites': true,
+      'user pages': true,
+      'topics': true,
+      'types to show': {
+        'popular': 'subsites and blogs',
+        'new': 'subsites and blogs',
+        'my new': 'subsites and blogs',
+        'bookmarks': 'subsites and blogs',
+        'subsites': 'subsites and blogs',
+        'user pages': 'subsites and blogs',
+        'topics': 'subsites and blogs'
+      },
+    }
   },
   'feeds': {
+    'what to group': {
+      'subsites': true,
+      'blogs': true
+    },
+    'filters': {
+      'ignored': {
+        'feeds': {
+          'action': 'collapse'
+        },
+        'authors': {
+          'action': 'collapse'
+        },
+        'subsites': {
+          'action': 'collapse'
+        },
+        'visual': {
+          'color': {
+            'panel': '#6c2e4c',
+            'feed': '#e5c6c6'
+          }
+        }
+      },
+      'watched': {
+        'feeds': {
+          'action': 'collapse'
+        },
+        'visual': {
+          'color': {
+            'panel': '',
+            'feed': '#c7e5d7'
+          }
+        }
+      },
+      'favorite': {
+        'visual': {
+          'color': {
+            'panel': '#a3f9c3',
+            'feed': '#a3f9c3'
+          }
+        }
+      },
+      'subsites': {
+        'title': {
+          'active': true,
+          'action': 'collapse',
+          'react text': true,
+          'react no text': true,
+          'words': []
+        },
+        'text': {
+          'active': true,
+          'action': 'collapse',
+          'react text': true,
+          'react no text': true,
+          'words': []
+        }
+      },
+      'blogs': {
+        'title': {
+          'active': true,
+          'action': 'collapse',
+          'react text': true,
+          'react no text': true,
+          'words': []
+        },
+        'text': {
+          'active': true,
+          'action': 'collapse',
+          'react text': true,
+          'react no text': true,
+          'words': []
+        }
+      }
+    },
     'attachments': {
       'replacing': {
         'video': true,
@@ -29,80 +103,6 @@ const defaultSettings = {
         'video': 300
       }
     }
-  },
-  'what to group': {
-    'subsites': true,
-    'blogs': true
-  },
-  'filters': {
-    'ignored': {
-      'feeds': {
-        'action': 'collapse'
-      },
-      'authors': {
-        'action': 'collapse'
-      },
-      'subsites': {
-        'action': 'collapse'
-      },
-      'visual': {
-        'color': {
-          'panel': '#6c2e4c',
-          'feed': '#e5c6c6'
-        }
-      }
-    },
-    'watched': {
-      'feeds': {
-        'action': 'collapse'
-      },
-      'visual': {
-        'color': {
-          'panel': '',
-          'feed': '#c7e5d7'
-        }
-      }
-    },
-    'favorite': {
-      'visual': {
-        'color': {
-          'panel': '#a3f9c3',
-          'feed': '#a3f9c3'
-        }
-      }
-    },
-    'subsites': {
-      'title': {
-        'active': true,
-        'action': 'collapse',
-        'react text': true,
-        'react no text': true,
-        'words': []
-      },
-      'text': {
-        'active': true,
-        'action': 'collapse',
-        'react text': true,
-        'react no text': true,
-        'words': []
-      }
-    },
-    'blogs': {
-      'title': {
-        'active': true,
-        'action': 'collapse',
-        'react text': true,
-        'react no text': true,
-        'words': []
-      },
-      'text': {
-        'active': true,
-        'action': 'collapse',
-        'react text': true,
-        'react no text': true,
-        'words': []
-      }
-    },
   },
   'script data': {
     'ignored': {
