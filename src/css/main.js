2cc1800abe5aaf870c96e7b7bf2c3885fa54f2ba
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-04-27 14:58:52 +0300

    Update main.js

--- src/css/main.js
+++ src/css/main.js
@@ -592,6 +592,11 @@ const mainCSS = (cfg) => `
 }
 
 
+.feed__item.l-island-round.editor .content-header-author--subsite {
+  background-color: ${cfg['feeds']['settings']['visual']['editor']['color']['feed']};
+  border-radius: 10px;
+  box-shadow: 0 0 2px 0px rgb(0 0 0);
+}
 
 .dtf-feedGroups .cont {
   display: flex;
